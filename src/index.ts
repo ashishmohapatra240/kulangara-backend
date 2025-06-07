dd6c629e5b192a9b533310436aacbe01dbaf89f2
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-06-07 16:40:55 +0530

    feat(routes): add product and category routes, and integrate middleware for logging, security, and error handling

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,37 @@ import { prisma } from './config/db';
 import redis from './config/redis';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route';
+import productRoutes from './routes/product.route';
+import categoryRoutes from './routes/category.route';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { errorHandler, notFoundHandler } from './middleware/error';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+// Middleware
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
-
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello kulangara');
 });
 
-
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/categories', categoryRoutes);
 
+// Handle 404
+app.use(notFoundHandler);
 
+// Error handling middleware
+app.use(errorHandler);
 
 app.listen(port, async () => {
     try {
